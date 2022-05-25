8576a4a93d97b2fcfb0bed0154fd1d334b417470
Rafael Almirante <88936758+almirantpromob7@users.noreply.github.com>
2022-05-25 13:44:12 -0300

    Arquivos adicionados via Upload

---
+++ Triangle2.java
@@ -0,0 +1,16 @@
+package entities;
+
+public class Triangle2 {
+		
+		public double a;
+		public double b;
+		public double c;
+		
+		public double area() {
+			double p = (a + b + c) / 2.0;
+			return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+			
+			/*posso usa o return result no final ou usar do modo que está;*/
+		}
+		
+}
